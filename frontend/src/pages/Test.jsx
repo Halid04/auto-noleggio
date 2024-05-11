07459d6872a9f933e9837f188ad1c4263e88fc5d
Halid Cisse <halidcisse346@gmail.com>
2024-05-11 16:57:37 +0200

    chore: Update dependencies and remove unused CSS styles

---
+++ frontend/src/pages/Test.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CardAuto from "../components/CardAuto";
+
+function Test() {
+  return (
+    <div>
+      <h1 className="text-4xl text-center mt-20">TEST!!</h1>
+      <CardAuto />
+    </div>
+  );
+}
+
+export default Test;
