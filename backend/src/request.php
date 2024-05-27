3e5bbf1cd360b02a8fc18a7da6f189da6c4e3431
panettondoro <70624290@itiscatelli.onmicrosoft.com>
2024-05-27 11:17:14 +0200

    Fixed CORS errors

--- backend/src/request.php
+++ backend/src/request.php
@@ -1,14 +1,26 @@
 <?php
     $contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
-    
+    /*
     if ($contentType !== 'application/json') {
         http_response_code(415);
         echo json_encode(["message" => "Only JSON content is supported"]);
         exit();
     }
+    */
     
     
     $requestMethod = $_SERVER['REQUEST_METHOD'];
+    /*
+    
+    */
+
+    // Allow from any origin
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+        header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+        exit(0);
+    }
 
     if ($requestMethod == "GET") {
         $data = json_decode($_GET['json'], true);
@@ -23,26 +35,18 @@
         exit();
     }
 
-    // Allow from any origin
-    if (isset($_SERVER['HTTP_ORIGIN'])) {
-        // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
-        // you want to allow, and if so:
-        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-        header('Access-Control-Allow-Credentials: true');
-        header('Access-Control-Max-Age: 86400');    // cache for 1 day
-    }
+    header("Access-Control-Allow-Origin: *");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');
     
+    /*
     // Access-Control headers are received during OPTIONS requests
-    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-        
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
             // may also be using PUT, PATCH, HEAD etc
-            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-        
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-    
-
-    }
+            header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
+       
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+    */
     
 ?>
