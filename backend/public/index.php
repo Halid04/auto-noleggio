8da930025bc2dfdb776d8dad82d81adb330e5e7e
panettondoro <70624290@itiscatelli.onmicrosoft.com>
2024-05-27 10:43:49 +0200

    CORS

--- backend/public/index.php
+++ backend/public/index.php
@@ -11,10 +11,11 @@
     
 
     header("Access-Control-Allow-Origin: *");
+    
     header("Content-Type: application/json; charset=UTF-8");
     header("Access-Control-Allow-Methods: GET,POST,PUT,DELETE");
     header("Access-Control-Max-Age: 3600");
-    //header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
     $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     $uri = array_slice(explode( '/', $uri ), 4);
